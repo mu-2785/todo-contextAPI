dfff6d972e01bcefa92df0ef969755b515b26471
mu-2785 <160841258+mu-2785@users.noreply.github.com>
2024-03-01 16:10:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,1 @@
-import AddTask from "./components/AddTask";
-import TaskList from "./components/TaskList";
-import "./App.css"
 
-const App = () => {
-  return (
-    <center className="m_app">
-      <AddTask/>
-      <TaskList/>
-    </center>
-  );
-};
-export default App;
